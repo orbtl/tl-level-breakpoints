34ec5de9884fd06ed97941f47c3d66b43f64bdb1
Jake Sklarew <twinsendude@gmail.com>
2025-02-25 14:46:14 -0800

    Starting to build models

---
+++ src/State/Stats/useStatStore.tsx
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+import statsObject from './TLBaseMainStat.json';
+
+statsObject
+function formatStats(statsObject)
+
+interface StatState {
+
+}
+
+const useStatStore = create<StatState>(set => {
+  
+});
